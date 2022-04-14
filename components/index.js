c63617b424953b9b323c07822e6db898e889fbf7
BLVCK <KoladeBalogun@BLVCKs-MacBook-Pro.local>
2022-04-14 03:45:08 +0100

    first-commit

--- components/index.js
+++ components/index.js
@@ -3,9 +3,8 @@ import ProfileDetailsCard from "./ProfileDetailsCard";
 import HomeHeader from "./HomeHeader";
 import { CircleButton, RectButton } from "./Button";
 import ProfileDesc from "./ProfileDesc";
-import DetailsBid from "./DetailsBid";
 import { SubInfo } from "./SubInfo";
 
 export {
-    FocusedStatusBar, ProfileDetailsCard, HomeHeader, CircleButton, RectButton, ProfileDesc, DetailsBid, SubInfo
+    FocusedStatusBar, ProfileDetailsCard, HomeHeader, CircleButton, RectButton, ProfileDesc, SubInfo
 }
